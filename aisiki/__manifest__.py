8d0dacf342ba16ef67ac9c6d7eac6629faff3c38
Ajepe <ajepebabatope@gmail.com>
2022-01-09 13:13:51 +0100

    [ADD] add restful module

---
+++ aisiki/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    "name": "Aisiki Connector",
+    "summary": """Aisiki Connector""",
+    "description": """
+        Aisiki Connector
+    """,
+    "author": "My Company",
+    "website": "http://www.yourcompany.com",
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    "category": "Uncategorized",
+    "version": "0.1",
+    # any module necessary for this one to work correctly
+    "depends": ["base", "sale_management", "account_accountant", "stock", "hr", "restful"],
+    # always loaded
+    "data": [
+        # 'security/ir.model.access.csv',
+        "views/views.xml",
+        "views/templates.xml",
+    ],
+    # only loaded in demonstration mode
+    "demo": ["demo/demo.xml",],
+}
