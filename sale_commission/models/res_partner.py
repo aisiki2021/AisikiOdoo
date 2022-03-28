b6eabdaf60f3ce68dfbcdc39a185bdbb61e0a059
Ajepe <ajepebabatope@gmail.com>
2022-03-28 15:18:10 +0100

    [UPD] Force update

--- sale_commission/models/res_partner.py
+++ sale_commission/models/res_partner.py
@@ -29,7 +29,7 @@ class ResPartner(models.Model):
         help="This is the default commission used in the sales where this "
         "agent is assigned. It can be changed on each operation if "
         "needed.",
-        default=1,
+        default=lambda r: r.env.ref('sale_commission.commission_001', raise_if_not_found=False).id if r.env.ref('sale_commission.commission_001', raise_if_not_found=False) else False,
     )
     settlement = fields.Selection(
         selection=[("monthly", "Monthly"), ("quaterly", "Quarterly"), ("semi", "Semi-annual"), ("annual", "Annual"),],
