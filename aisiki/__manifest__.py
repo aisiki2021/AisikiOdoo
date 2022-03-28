7d4eed339753ffea69c9d02a782aad2d938dd8cd
Babatope Ajepe <ajepebabatope@gmail.com>
2022-03-28 14:32:04 +0100

    Update __manifest__.py

--- aisiki/__manifest__.py
+++ aisiki/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "Babatope Ajepe",
     "website": "http://www.yourcompany.com",
     "category": "Uncategorized",
-    "version": "1.4.1",
+    "version": "1.1",
     "depends": [
         "base",
         "sms",
