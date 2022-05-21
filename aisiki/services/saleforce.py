7c3d80bed2dc505f8a1d4b6e2bfd60d6f8a8c1b3
Babatope Ajepe <ajepebabatope@gmail.com>
2022-05-21 20:52:09 +0100

    [UPD] update

--- aisiki/services/saleforce.py
+++ aisiki/services/saleforce.py
@@ -138,6 +138,7 @@ class OrderingApp(Component):
             "emergency_address": payload.emergency_address,
             "emergency_state": payload.emergency_state,
             "emergency_city": payload.emergency_city,
+            "image_1024": payload.image,
         }
 
         try:
